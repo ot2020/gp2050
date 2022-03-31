d9854ef52cdfcee2e368214934109526fb0f5da6
Olzhas Tolegen <turatam@hotmail.com>
2022-03-31 20:50:06 +0600

    base ASP.NET Core uploaded

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gp2050.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
